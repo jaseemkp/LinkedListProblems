e87092ebe7fea3c4b738128b11cfc305e2c1e3a2
jaseemjas <jasmedia.jas@gmail.com>
2012-10-16 23:32:44 +0530

    Sorted Insert

--- 6_sortedInsrt.c
+++ 6_sortedInsrt.c
@@ -1,5 +1,9 @@
 /**************************************************************************
-Write a SortedInsert() function which given a list that is sorted in increasing order, and a single node, insert the node into the correct sorted position in the list.While Push() allocates a new node to add to the list, SortedInsert() takes the existing node, and just rearranges pointers to insert it into the list.
+Write a SortedInsert() function which given a list that is sorted in 
+increasing order, and a single node, insert the node into the correct sorted
+ position in the list.While Push() allocates a new node to add to the list, 
+SortedInsert() takes the existing node, and just rearranges pointers to 
+insert it into the list.
 ***************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
