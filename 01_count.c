fd09771c223478a05515e3d409f9e0974ddbe270
jaseemjas <jasmedia.jas@gmail.com>
2012-10-18 20:07:23 +0530

    Count

---
+++ 01_count.c
@@ -0,0 +1,53 @@
+/**************************************************************************
+ write Count() function that counts the number of times a given int occurs
+ in a list. The code for this has the classic list traversal structure as 
+demonstarated in Lenght()
+***************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node* next;
+};
+/*------------BuildOneTwoThree-------------*/
+struct node* buildonetwothree(){
+	struct node* head = NULL;
+	struct node* second = NULL;
+	struct node* third = NULL;
+
+	head = malloc(sizeof(struct node));
+	second = malloc(sizeof(struct node));
+	third = malloc(sizeof(struct node));
+
+	head -> data = 1;
+	head -> next = second;
+
+	second -> data = 2;
+	second -> next = third;
+
+	third -> data = 3;
+	third -> next = NULL;
+
+	return head;
+}
+/*--------------Test Driver---------------*/
+void main(){
+	struct node*  myList = buildonetwothree(); // {1, 2, 3}
+
+	int count = Count(myList, 2);
+	printf("%d\n", count);
+}
+
+/*------------COUNT----------------*/
+int Count(struct node* head, int search){
+	struct node* current = head;
+	int count = 0;
+	
+	for(current = head; current != NULL; current = current->next){
+		if(current->data == search)
+		count++;
+		}
+	return count;
+}
+
+
